28f5bfcc07dbe05a36af78016426c7c0e2b8d41b
Irwnda <irwandaa7@gmail.com>
2025-05-24 16:48:34 +0700

    feat: OSN-P 2024 A

---
+++ 2024/A/jawaban.h
@@ -0,0 +1,15 @@
+//
+// Created by cahInfor on 24/05/25.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <string>
+
+using namespace std;
+
+int count_length(const string &start, const string &s);
+
+int solve(const string& s);
+
+#endif //MAIN_H
